7b750681978babf14fd4c436d75db3a4e62a37a8
Lionel Duboeuf <lduboeuf@ouvaton.org>
2026-04-09 10:49:30 +0200

    Allow initializing SeasideCache from client. Add populatedChanged signal

--- lib/seasidecache.h
+++ lib/seasidecache.h
@@ -358,6 +358,8 @@ public:
     static int importContacts(const QString &path);
     static QString exportContacts();
 
+    static void initialize(FetchDataType requiredTypes = FetchNone,
+                           FetchDataType extraTypes = FetchNone);
     static const QList<quint32> *contacts(FilterType filterType);
     static bool isPopulated(FilterType filterType);
 
@@ -394,6 +396,9 @@ public:
         removeRange(m_syncFilter, index, count);
         return 0;
     }
+
+signals:
+    void populatedChanged();
 
 protected:
     void timerEvent(QTimerEvent *event);
